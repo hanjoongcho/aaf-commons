b09f68e5dc68e2e9b46b846337780302359576ae
hanjoong.cho <hanjoongcho@gmail.com>
2017-12-29 00:48:10 +0900

    Initial commit

--- commons/src/main/kotlin/io/github/hanjoongcho/commons/activities/BaseSimpleActivity.kt
+++ commons/src/main/kotlin/io/github/hanjoongcho/commons/activities/BaseSimpleActivity.kt
@@ -1,13 +1,8 @@
 package io.github.hanjoongcho.commons.activities
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-import com.simplemobiletools.commons.activities.AboutActivity
-import com.simplemobiletools.commons.helpers.APP_LICENSES
-import com.simplemobiletools.commons.helpers.APP_NAME
-import com.simplemobiletools.commons.helpers.APP_VERSION_NAME
 import io.github.hanjoongcho.commons.R
 
 /**
@@ -28,27 +23,6 @@ open class BaseSimpleActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
     }
-    
-    override fun onPause() {
-        super.onPause()
-//        CommonUtils.saveLongPreference(this@BaseSimpleActivity, SETTING_PAUSE_MILLIS, System.currentTimeMillis())
-    }
-
-    override fun onResume() {
-        super.onResume()
-//        val pauseMillis = CommonUtils.loadLongPreference(this@BaseSimpleActivity, SETTING_PAUSE_MILLIS, 0L)
-//        Log.i(IntroActivity.TAG, "pauseMillis: ${System.currentTimeMillis() - pauseMillis}")
-//        if (pauseMillis != 0L) {
-//            if (System.currentTimeMillis() - pauseMillis > 1000) {
-//                val intent = Intent(this@BaseSimpleActivity, PatternLockActivity::class.java).apply {
-//                    putExtra(PatternLockActivity.MODE, PatternLockActivity.UNLOCK_RESUME)
-////                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-//                }
-//                EasyPasswordHelper.startSettingActivityWithTransition(this@BaseSimpleActivity, intent)
-//            }
-//        }
-//        initTextSize(findViewById(android.R.id.content), this@BaseSimpleActivity);
-    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
@@ -57,18 +31,5 @@ open class BaseSimpleActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    fun startAboutActivity(appNameId: Int, licenseMask: Int, versionName: String) {
-        Intent(applicationContext, AboutActivity::class.java).apply {
-            putExtra(APP_NAME, getString(appNameId))
-            putExtra(APP_LICENSES, licenseMask)
-            putExtra(APP_VERSION_NAME, versionName)
-            startActivity(this)
-        }
-    }
-
-    companion object {
-        const val SETTING_PAUSE_MILLIS = "setting_pause_millis"
     }
 }
