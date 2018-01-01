c8e8f4593c7c74c2d11fbffa6532c15ed01efe0f
hanjoong.cho <hanjoongcho@gmail.com>
2018-01-01 13:57:31 +0900

    Implement common activities

--- commons-android/src/main/kotlin/io/github/hanjoongcho/commons/helpers/BaseConfig.kt
+++ commons-android/src/main/kotlin/io/github/hanjoongcho/commons/helpers/BaseConfig.kt
@@ -1,6 +1,7 @@
 package io.github.hanjoongcho.commons.helpers
 
 import android.content.Context
+import android.preference.PreferenceManager
 
 /**
  * Created by CHO HANJOONG on 2017-12-30.
@@ -10,6 +11,8 @@ import android.content.Context
  */
 
 open class BaseConfig(context: Context) : com.simplemobiletools.commons.helpers.BaseConfig(context) {
+    private val legacyPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+
     companion object {
         fun newInstance(context: Context) = BaseConfig(context)
     }
@@ -18,12 +21,12 @@ open class BaseConfig(context: Context) : com.simplemobiletools.commons.helpers.
         get() = prefs.getBoolean(AAF_PATTERN_LOCK_PROTECTION_ON, false)
         set(appPasswordProtectionOn) = prefs.edit().putBoolean(AAF_PATTERN_LOCK_PROTECTION_ON, appPasswordProtectionOn).apply()
 
-    
+
     var aafPatternLockPauseMillis: Long
         get() = prefs.getLong(AAF_PATTERN_LOCK_PAUSE_MILLIS, 0L)
         set(aafPatternLockPauseMillis) = prefs.edit().putLong(AAF_PATTERN_LOCK_PAUSE_MILLIS, aafPatternLockPauseMillis).apply()
-    
+
     var aafPatternLockSaved: String
-        get() = prefs.getString(AAF_PATTERN_LOCK_SAVED, AAF_PATTERN_LOCK_DEFAULT)
-        set(aafPatternLockSaved) = prefs.edit().putString(AAF_PATTERN_LOCK_SAVED, aafPatternLockSaved).apply()
+        get() = legacyPrefs.getString(AAF_PATTERN_LOCK_SAVED, AAF_PATTERN_LOCK_DEFAULT)
+        set(aafPatternLockSaved) = legacyPrefs.edit().putString(AAF_PATTERN_LOCK_SAVED, aafPatternLockSaved).apply()
 }
