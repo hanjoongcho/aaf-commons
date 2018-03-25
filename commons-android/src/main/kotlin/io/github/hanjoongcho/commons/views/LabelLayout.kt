e6793b510af0022e1c6ca733227ee60d0c682c94
hanjoong.cho <hanjoongcho@gmail.com>
2018-03-26 07:39:48 +0900

    Refactoring

---
+++ commons-android/src/main/kotlin/io/github/hanjoongcho/commons/views/LabelLayout.kt
@@ -0,0 +1,21 @@
+package io.github.hanjoongcho.commons.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * Created by CHO HANJOONG on 2017-12-19.
+ */
+
+class LabelLayout : LinearLayout {
+    constructor(context: Context) : super(context) 
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) 
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) 
+
+    fun setColors(textColor: Int, accentColor: Int, backgroundColor: Int) {
+        setBackgroundColor(accentColor)
+    }
+}
